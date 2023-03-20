b9106135353980f5854ebfe6241a1efa23faf446
aboutmydreams <aboutmydreams@163.com>
2023-03-20 21:40:22 +0800

    feat: init project and fix bug

--- src/store/appSetting/context.tsx
+++ src/store/appSetting/context.tsx
@@ -2,9 +2,11 @@ import { createContext, ReactNode, useCallback, useReducer } from 'react';
 import {
   Actions,
   ActionsMap,
+  AdminOrg,
   AppSettingReducer,
   AppSettingState,
   Auth,
+  Claim,
   I18n,
   Theme
 } from './reducer';
@@ -28,13 +30,31 @@ const initI18n: I18n = {
 };
 
 const initAuth: Auth = {
-  address: '',
-  uid: '',
-  role: 0
+  address: localStorage.getItem('address') || '',
+  token: localStorage.getItem('token') || '',
+  name: localStorage.getItem('name') || '',
+  avatar: localStorage.getItem('avatar') || '',
+  uid: localStorage.getItem('uid') ? Number(localStorage.getItem('uid')) : 0,
+  isInWhiteList:
+    localStorage.getItem('isInWhiteList') === 'true' ? true : false,
+  popup: Number(localStorage.getItem('popup')) || 0,
+  number: Number(localStorage.getItem('number')) || 0
 };
 
+const initClaim: Claim = {
+  toClaim: 0
+};
+
+const initAdminOrg: AdminOrg[] = [];
+
 export const AppSettingContext = createContext<AppSettingContextInterface>([
-  { theme: initTheme, i18n: initI18n, auth: initAuth },
+  {
+    theme: initTheme,
+    i18n: initI18n,
+    auth: initAuth,
+    claim: initClaim,
+    adminOrg: initAdminOrg
+  },
   () => null
 ]);
 
@@ -42,7 +62,9 @@ export function AppSettingProvider({ children }: { children: ReactNode }) {
   const [state, _dispatch] = useReducer(AppSettingReducer, {
     theme: initTheme,
     i18n: initI18n,
-    auth: initAuth
+    auth: initAuth,
+    claim: initClaim,
+    adminOrg: initAdminOrg
   });
 
   const dispatch: Dispatcher = useCallback((type, ...payload) => {
--- src/store/appSetting/index.tsx
+++ src/store/appSetting/index.tsx
@@ -1,27 +1,41 @@
 import { useCallback, useContext } from 'react';
-import { Auth, Theme } from './reducer';
+import { AdminOrg, Auth, Claim, Theme } from './reducer';
 import { AppSettingContext } from './context';
 
 export function useAppSetting() {
-  const [{ theme, i18n, auth }, dispatch] = useContext(AppSettingContext);
+  const [{ theme, i18n, auth, claim, adminOrg }, dispatch] =
+    useContext(AppSettingContext);
 
   const setTheme = useCallback(
     (theme: Theme) => dispatch('setTheme', theme),
     []
   );
-
   const setAuth = useCallback((auth: Auth) => dispatch('setAuth', auth), []);
+  const setClaim = useCallback(
+    (claim: Claim) => dispatch('setClaim', claim),
+    []
+  );
+  const setAdminOrg = useCallback(
+    (adminOrg: AdminOrg[]) => dispatch('setAdminOrg', adminOrg),
+    []
+  );
 
   return {
     appSetting: {
       theme,
       i18n,
-      auth
+      auth,
+      claim,
+      adminOrg
     },
     theme,
     i18n,
     auth,
+    claim,
+    adminOrg,
     setTheme,
-    setAuth
+    setAuth,
+    setClaim,
+    setAdminOrg
   };
 }
